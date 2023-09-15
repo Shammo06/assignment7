957f24032a3a1d2fc708bf964e2f5ee4508878fb
Shammo06 <mrshammo2018@gmail.com>
2023-09-16 00:55:38 +0600

    fix icon and card size

--- src/component/courses.jsx
+++ src/component/courses.jsx
@@ -2,16 +2,16 @@ import './courses.css'
 const Courses = ({courses}) => {
     
     const {name, description, icon, price, credit} = courses;
-    console.log(courses)
-    console.log(name)
     return (
         <div className='card'>
-            <img className='' src={icon} alt="" />
-            <h5>{name}</h5>
-            <p className=''>{description}</p>
-            <p>Price : {price}</p>
-            <p>Credit: {credit}hr</p>
-            <button>Select</button>            
+            <img src={icon} alt="" />
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <div className='value'>
+                <p>Price : {price}</p>
+                <p>Credit: {credit}hr</p>
+            </div>            
+            <button>Select</button> 
         </div>
     );
 };
